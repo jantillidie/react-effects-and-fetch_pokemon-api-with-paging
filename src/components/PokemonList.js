b6246ef450fe7634b87a51f8e04b547f0e11863e
Jan Maaß <git@antipost.de>
2023-11-17 12:16:17 +0100

    Add solution

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,27 +2,41 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [nextUrl, setNextUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [prevUrl, setPrevUrl] = useState(0);
+
+  const loadPokemon = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPokemon(data.results);
+      setNextUrl(data.next);
+      setPrevUrl(data.previous);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    async function loadPokemon() {
-      try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
-        const data = await response.json();
-        setPokemon(data.results);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    loadPokemon();
+    loadPokemon("https://pokeapi.co/api/v2/pokemon");
   }, []);
+
+  const handlePrevious = () => {
+    if (prevUrl) {
+      loadPokemon(prevUrl);
+    }
+  };
+
+  const handleNext = () => {
+    if (nextUrl) {
+      loadPokemon(nextUrl);
+    }
+  };
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={handlePrevious} disabled={!prevUrl}>Previous Page</button>
+      <button type="button" onClick={handleNext} disabled={!nextUrl}>Next Page</button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
